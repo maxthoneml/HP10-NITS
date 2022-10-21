057a65d24940e71dfe5df5b7758045fdb1767bf5
Aditya Bodhankar <adityaabodhankar@gmail.com>
2022-10-21 20:43:11 +0530

    init

---
+++ app/src/main/java/ml/maxthone/hp10_nits/MainActivity.java
@@ -0,0 +1,14 @@
+package ml.maxthone.hp10_nits;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
