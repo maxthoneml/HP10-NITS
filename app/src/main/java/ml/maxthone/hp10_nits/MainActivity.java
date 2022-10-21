bc1617885069ee415ba096c3c68929c3af61c639
Aditya Bodhankar <adityaabodhankar@gmail.com>
2022-10-21 22:01:16 +0530

    login ui

--- app/src/main/java/ml/maxthone/hp10_nits/MainActivity.java
+++ app/src/main/java/ml/maxthone/hp10_nits/MainActivity.java
@@ -1,14 +1,116 @@
 package ml.maxthone.hp10_nits;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+import ml.maxthone.hp10_nits.Models.ModelUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText emailInput, passwordInput;
+    private TextView loginBtn, signUpBtn, googleBtn;
+    private ProgressDialog pd;
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        emailInput = findViewById(R.id.login_email);
+        passwordInput = findViewById(R.id.login_password);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        pd = new ProgressDialog(this);
+        pd.setTitle("Logging in");
+        pd.setCancelable(false);
+        //check for login state
+        if(auth.getCurrentUser()!= null){
+            //user is logged in
+            //nav to another page as per role
+            pd.setMessage("Processing your request...");
+            pd.show();
+            validateUserType();
+            return;
+        }
+
+
+        loginBtn = findViewById(R.id.login_btn);
+        googleBtn = findViewById(R.id.google_sign_in_btn);
+        signUpBtn = findViewById(R.id.sign_up_nav);
+
+
+        loginBtn.setOnClickListener(v -> loginUser());
+    }
+
+    private void loginUser(){
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        if(!validateData(email, password)){
+            return;
+        }
+        pd.setMessage("Processing your request...");
+        pd.show();
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
+            Toast.makeText(this, "User Logged in successfully", Toast.LENGTH_SHORT).show();
+            validateUserType();
+        }).addOnFailureListener(e -> {
+            Toast.makeText(this, "Unable to log in user.", Toast.LENGTH_SHORT).show();
+            Log.e("LOGIN ERROR",e.getMessage());
+            pd.dismiss();
+        });
+    }
+
+    private void validateUserType() {
+        pd.setMessage("Validating your data");
+        String uid = Objects.requireNonNull(auth.getCurrentUser()).getUid();
+        db.collection("users").document(uid).get().addOnSuccessListener(documentSnapshot -> {
+            if(documentSnapshot.exists()){
+                ModelUser user = documentSnapshot.toObject(ModelUser.class);
+                Toast.makeText(this, "User name :- "+user.getName(), Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(MainActivity.this, "Error to get user data.", Toast.LENGTH_SHORT).show();
+                //sign out user
+                auth.signOut();
+                pd.dismiss();
+            }
+        }).addOnFailureListener(e -> {
+            Toast.makeText(MainActivity.this, "Error to get user data.", Toast.LENGTH_SHORT).show();
+            //sign out user
+            auth.signOut();
+            pd.dismiss();
+        });
+    }
+
+    private boolean validateData(String email, String password) {
+        if(email.equalsIgnoreCase("")){
+            emailInput.setError("Email required");
+            return false;
+        }
+        if(password.equalsIgnoreCase("")){
+            passwordInput.setError("Password required");
+            return false;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/ml/maxthone/hp10_nits/Models/ModelUser.java
@@ -0,0 +1,65 @@
+package ml.maxthone.hp10_nits.Models;
+
+public class ModelUser {
+    private String uid, email, name, phone, role, profileImage;
+
+    public ModelUser() {
+    }
+
+    public ModelUser(String uid, String email, String name, String phone, String role, String profileImage) {
+        this.uid = uid;
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+        this.role = role;
+        this.profileImage = profileImage;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+}
