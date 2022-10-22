6a3e6831b41d89018d9512532a1899059f26e8b5
asyncdoggo <ayushdeshpande81@gmail.com>
2022-10-22 10:21:18 +0530

    Fix minor issues

--- app/src/main/java/ml/maxthone/hp10_nits/MainActivity.java
+++ app/src/main/java/ml/maxthone/hp10_nits/MainActivity.java
@@ -1,24 +1,16 @@
 package ml.maxthone.hp10_nits;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Objects;
